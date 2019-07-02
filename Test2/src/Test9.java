b5f811cc7fc57153162ecfdb2bae55e76e706d89
rama <rama911@gmail.com>
2019-07-02 16:35:44 +0800

    First commit

---
+++ Test2/src/Test9.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\rama\\Documents\\Tools\\geckodriver\\geckodriver.exe");
+		 
+		 FirefoxDriver driver = new FirefoxDriver();
+		 
+		 driver.get("http://www.google.com");
+		 
+		 driver.manage().window().maximize();
+
+	}
+
+}
